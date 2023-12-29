23e405b55019c6f8dd5d95364c0bc58530bfd246
Shaun Stoltz <shaunstoltz@hotmail.com>
2023-12-29 11:46:27 -0500

    fixes

--- run_clm_no_trainer.py
+++ run_clm_no_trainer.py
@@ -568,17 +568,18 @@ def main():
 
             if isinstance(checkpointing_steps, int):
                 if completed_steps % checkpointing_steps == 0:
-                    model.eval()
+                    #model.eval()
                     output_dir = f"step_{completed_steps}"
                     if args.output_dir is not None:
                         output_dir = os.path.join(args.output_dir, output_dir)
                     #accelerator.save_state(output_dir)
+                    torch.cuda.empty_cache()
                     accelerator.wait_for_everyone()
                     unwrapped_model = accelerator.unwrap_model(model)
                     unwrapped_model.save_pretrained(args.output_dir, save_function=accelerator.save)
                     if accelerator.is_main_process:
                         tokenizer.save_pretrained(args.output_dir)
-                    model.train()
+                    #model.train()
             if completed_steps >= args.max_train_steps:
                 break
 
